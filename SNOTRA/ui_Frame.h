23380172c849c426f746dfd15b52f35cc40c1929
marc <marc@Marc-PC>
2013-03-25 16:27:41 +0100

    juste comit the FRAME.ui

--- SNOTRA/ui_Frame.h
+++ SNOTRA/ui_Frame.h
@@ -1,7 +1,7 @@
 /********************************************************************************
 ** Form generated from reading UI file 'Frame.ui'
 **
-** Created: Mon Mar 25 15:13:52 2013
+** Created: Mon Mar 25 15:35:10 2013
 **      by: Qt User Interface Compiler version 4.8.3
 **
 ** WARNING! All changes made in this file will be lost when recompiling UI file!
